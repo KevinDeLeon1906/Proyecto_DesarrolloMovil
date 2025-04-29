99bdbbc06c0b92fcc31332bfdc0ed7feeb047cf1
fernandodeleon1906@outlook.com <KevinDeLeon1906>
2025-04-28 22:12:20 -0600

    Pantalla de carga inicial

---
+++ lib/constantes.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+
+const Color rosa = Color(0xFFE4007C);
+const Color azul = Color(0xFF73C2FB);
--- lib/pantalla_carga_inicial.dart
+++ lib/pantalla_carga_inicial.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class PantallaCargaInicial extends StatefulWidget {
   const PantallaCargaInicial({super.key});
@@ -9,9 +9,32 @@ class PantallaCargaInicial extends StatefulWidget {
 
 class _PantallaCargaInicialState extends State<PantallaCargaInicial> {
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+       Navigator.of(context).pushReplacement(
+         MaterialPageRoute(builder: (context) => const TuSiguientePantalla()),
+       );
+    });
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Color rosa brillante similar al de la imagen
+      backgroundColor: const Color(0xFFFF0099),
+      body: Center(
+        child: Text(
+          'NUMI',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            fontFamily: 'Katibeh'
+          ),
+        ),
+      ),
     );
   }
 }
