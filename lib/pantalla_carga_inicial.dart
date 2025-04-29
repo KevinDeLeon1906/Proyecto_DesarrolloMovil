85af63b24747f13f751f0c06cb1ade5e4830c2bf
fernandodeleon1906@outlook.com <KevinDeLeon1906>
2025-04-28 21:48:35 -0600

    Initial commit

---
+++ lib/pantalla_carga_inicial.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class PantallaCargaInicial extends StatefulWidget {
+  const PantallaCargaInicial({super.key});
+
+  @override
+  State<PantallaCargaInicial> createState() => _PantallaCargaInicialState();
+}
+
+class _PantallaCargaInicialState extends State<PantallaCargaInicial> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
